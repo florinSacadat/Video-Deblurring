de499c9b6dbc2828734d74c14691071ce9271fa5
florinSacadat <52105797+florinSacadat@users.noreply.github.com>
2020-03-11 20:37:10 +0200

    All in one script to process a video with the NN
    the process if Full HD video->Hd Video->Frame separation->Index listing->model testing with the current best checkpoint(long process)->video result assembly

---
+++ Video-Frame-TEST-Video.py
@@ -0,0 +1,103 @@
+import cv2
+import os
+import glob
+import os
+import torch
+from torch.utils.data import DataLoader
+import DeBlurNet as M
+import DatasetTEST as D
+import VisdomUtils
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+import utils.Metrics as metrics
+import Frames2Video
+
+def Vid2Frames_Index(vid_link):
+    out_link=vid_link[:-4]+"/InputFrames/"
+    if not os.path.isdir(out_link):
+        os.makedirs(out_link)
+    file = open(out_link+"5Frames.txt", "w+")
+    vidcap = cv2.VideoCapture(vid_link)
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    while success:
+        cv2.imwrite(out_link+"/%s.png" % count.__str__().zfill(4),
+                    image)  # save frame as png file
+        success, image = vidcap.read()
+
+        count += 1
+    imList = glob.glob(out_link + '*.png')
+    j = -1
+    while j + 5 < len(imList):
+        x1 = j + 1
+        x2 = j + 2
+        x3 = j + 3
+        x4 = j + 4
+        x5 = j + 5
+        file.write(
+           out_link+ '%04d.png' % x1 + ' , ' +    out_link + '%04d.png' % x2 + ' , ' +    out_link + '%04d.png' % x3 + ' , ' +   out_link + '%04d.png' % x4 + ' , ' +   out_link + '%04d.png' % x5 + '\n')
+        j = j + 1
+
+
+    file.close()
+
+    return out_link+"5Frames.txt"
+
+
+def test_function(model, test_loader):
+
+    psnr=0
+    model.eval()
+
+    for batch_idx, (inputs) in enumerate(test_loader):
+        inputs = inputs
+
+
+        in_img = inputs
+
+        out_img = model(in_img)
+        out_img = out_img
+
+
+        if not os.path.isdir(result_dir ):
+            os.makedirs(result_dir)
+
+        out_img=transforms.ToPILImage()(out_img['output1'][0].cpu())
+        # in_img[2].save(result_dir+'FlorinDS_MSE_FS_DBN-MSE_00_in_%d.jpg' % batch_idx)
+        out_img.save(result_dir +'%04d.jpg' % batch_idx)
+        # target_img.save(result_dir +'FlorinDS_MSE_FS_DBN-MSE_00_target_%d.jpg' %  batch_idx)
+        # psnr = metrics.PSNR(target_img, out_img)
+        # print("PSNR image%d = %d ",(batch_idx,psnr))
+
+    return 0
+
+
+if __name__ == '__main__':
+    vid_link = '/home/student/Documents/DeepVideoDebluring/PingPong.mp4'
+    format=1080
+    if format==1080:
+     converter = os.system(
+        'ffmpeg -i '+vid_link+' -vf scale=-1:720 -c:v libx264 -crf 18 -preset veryslow -c:a copy '+vid_link[:-4]+"HD.mp4")
+    vid_link = vid_link[:-4] + "HD.mp4"
+
+
+    print (Vid2Frames_Index(vid_link))
+
+    file_root_blur=Vid2Frames_Index(vid_link)
+    result_dir=vid_link[:-4]+"Results/"
+    if not os.path.isdir(result_dir):
+        os.makedirs(result_dir)
+    model_dir="/media/student/2.0 TB Hard Disk/modelsCkp/FlorinDS_MSE_FS/"
+    model_name="FS_checkpoint_FlorinDS_MSE_DBN_e0260"
+
+    test_set = D.FlorinDataset(file_root_blur,transforms)
+    test_loader = DataLoader(test_set, batch_size=1,shuffle=False,num_workers=6)
+
+    model=M.DeBlurNet()
+    model.load_state_dict(torch.load(model_dir+model_name))
+    test_function(model,test_loader)
+
+    Frames2Video.Frames2Video(result_dir,result_dir+"Result30fps.mp4")
+
